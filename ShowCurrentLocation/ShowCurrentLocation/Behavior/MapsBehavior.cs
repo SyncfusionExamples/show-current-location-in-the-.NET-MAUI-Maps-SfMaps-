96b9066fe0f6a1a5f4592dd4604ff0d2b9438b21
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2023-06-14 16:22:20 +0530

    Moved the code behind codes to behavior class

---
+++ ShowCurrentLocation/ShowCurrentLocation/Behavior/MapsBehavior.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.Maui.Maps;
+
+namespace ShowCurrentLocation
+{
+    public class MapsBehavior : Behavior<ContentPage>
+    {
+        private MapTileLayer tileLayer;
+        private MapMarker marker;
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.tileLayer = bindable.FindByName<MapTileLayer>("tileLayer");
+            this.marker = bindable.FindByName<MapMarker>("marker");
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            Location location = await Geolocation.GetLocationAsync(new GeolocationRequest
+            {
+                DesiredAccuracy = GeolocationAccuracy.High,
+                Timeout = TimeSpan.FromSeconds(30)
+            });
+
+            if (location != null)
+            {
+                tileLayer.Center = new MapLatLng(location.Latitude, location.Longitude);
+
+                marker.Latitude = location.Latitude;
+                marker.Longitude = location.Longitude;
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.tileLayer = null;
+            this.marker = null;
+        }
+    }
+}
--- ShowCurrentLocation/ShowCurrentLocation/MainPage.xaml.cs
+++ ShowCurrentLocation/ShowCurrentLocation/MainPage.xaml.cs
@@ -1,32 +1,10 @@
-﻿using Syncfusion.Maui.Maps;
-
-namespace ShowCurrentLocation;
+﻿namespace ShowCurrentLocation;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-        GetLocation();
-    }
-
-    private async void GetLocation()
-    {
-        Location location = await Geolocation.GetLocationAsync(new GeolocationRequest
-        {
-            DesiredAccuracy = GeolocationAccuracy.High,
-            Timeout = TimeSpan.FromSeconds(30)
-        });
-
-        if (location != null)
-        {
-            //tileLayer.Center.Latitude = location.Latitude;
-            //tileLayer.Center.Longitude = location.Longitude;
-            tileLayer.Center = new MapLatLng(location.Latitude, location.Longitude);
-
-            marker.Latitude = location.Latitude;
-            marker.Longitude = location.Longitude;
-        }
     }
 }
 
