82e8fffafd40bff951e6a3986c1a9e5dfb00263b
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2023-06-14 08:34:51 +0530

    New KB sample added and README file updated

---
+++ ShowCurrentLocation/ShowCurrentLocation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ShowCurrentLocation;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
